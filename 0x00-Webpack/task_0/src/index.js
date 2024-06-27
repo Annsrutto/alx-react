6cbaacceba8d5d2bb303a4353b48454ff2c353e5
Annsrutto <annsrutto567@gmail.com>
2024-06-28 01:58:41 +0300

    Basic setup

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,1 +1,7 @@
-const $ = require(jQuery);
+import $ from 'jquery';
+
+$(document).ready(function() {
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
+});
