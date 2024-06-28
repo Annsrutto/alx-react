e9c1153995c41c35f0a9a05c693a97c8354d909d
Annsrutto <annsrutto567@gmail.com>
2024-06-29 01:06:36 +0300

    Adding CSS & Images

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,26 +1,19 @@
 import $ from 'jquery';
 import _ from 'lodash';
 import '../css/main.css';
-import Logo from '../assets/holberton-logo.jpg';
+
+$('body').prepend('<div id="logo"></div>');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button id="clickButton">Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let count = 0;
 
 function updateCounter() {
-  let count = 0;
-  return function () {
-    count++;
-    $('#count').text(`${count} clicks on the button`);
-  };
+  count++;
+  $('#count').text(`${count} clicks on the button`);
 }
 
-$(document).ready(function() {
-  $('body').append('<div id="logo"></div>');
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button>Click here to get started</button>');
-  $('body').append('<p id="count"></p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
-
-  $('#logo').css('background-image', `url(${Logo})`);
-
-  const handleClick = _.debounce(updateCounter(), 300);
-  $('button').on('click', handleClick);
-});
+$('#clickButton').on('click', _.debounce(updateCounter, 500));
