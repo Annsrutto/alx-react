5c3d983d48fc3a06d421d0c4ebca1affb598b465
Annsrutto <annsrutto567@gmail.com>
2024-06-29 01:03:08 +0300

    Adding CSS & Images

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -2,44 +2,26 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './src/index.js',
+  entry: './js/dashboard_main.js',
   output: {
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-        loader: 'image-webpack-loader',
-        options: {
-          mozjpeg: {
-            progressive: true,
-            quality: 65
-          },
-          optipng: {
-            enabled: true
-          },
-          pngquant: {
-            quality: [0.65, 0.90],
-            speed: 4
-          },
-          gifsicle: {
-            interlaced: false
-          },
-          webp: {
-            quality: 75
-          }
-        }
-      }
-    ]
+      },
+    ],
   },
   performance: {
-    hints: false
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
   }
 };
