87ddc4a465366350939fdda6d4bf9fc2a1045a10
Annsrutto <annsrutto567@gmail.com>
2024-07-05 04:23:34 +0300

    Reorganize the files

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,3 +1,15 @@
-import './styles.css';
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-console.log('Hello from index.js');
+import App from "./App"
+import Notifications from "./Notifications";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+      <App />
+      <div className="root-notifications">
+        <Notifications />
+      </div>
+  </React.StrictMode>
+)
